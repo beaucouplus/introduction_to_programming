2f901c54f6f3e154065759955510988b6077e6e3
beaucouplus <maxime@beaucouplus.com>
2018-04-05 11:40:36 +0200

    done variable exercises

---
+++ variables/age.rb
@@ -0,0 +1,7 @@
+print "What's your age? "
+your_age = gets.chomp
+
+puts "In 10 years you will be #{your_age.to_i + 10 }"
+puts "In 20 years you will be #{your_age.to_i + 20 }"
+puts "In 30 years you will be #{your_age.to_i + 30 }"
+puts "In 40 years you will be #{your_age.to_i + 40 }"
---
+++ variables/name.rb
@@ -0,0 +1,7 @@
+print "What's your first_name? "
+first_name = gets.chomp
+print "What's your last_name? "
+last_name = gets.chomp
+10.times do
+  puts "Greetings #{first_name} #{last_name}!"
+end
